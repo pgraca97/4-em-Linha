307147696e32c116bb498fd50cea9851dccf7a32
pgraca97 <116584573+pgraca97@users.noreply.github.com>
2022-12-22 19:45:57 +0000

    Avanço + Menu do Jogo

---
+++ jogo.py
@@ -0,0 +1,164 @@
+print(1111111111)
+import numpy as np
+import pygame
+import sys
+import math
+
+
+cor_tabuleiro = (255,244,97)
+cor_fundo = (4,44,29)
+verde = (115, 255, 204)
+rosa = (243,129,199)
+
+cont_linha = 6
+cont_coluna = 7
+
+secc_tabuleiro = 100 #tamanho de cada círculo do tabuleiro
+width = cont_coluna * secc_tabuleiro
+height = (cont_linha+1) * secc_tabuleiro
+
+tamanho = (width, height)
+
+screen = pygame.display.set_mode(tamanho)
+
+def font(size):
+    #Devolve a font no tamanho que eu quiser
+    return pygame.font.Font("assets/font.ttf", size)
+
+def criar_tabuleiro():
+    tabuleiro = np.zeros ((cont_linha,cont_coluna)) #6 linhas por 7 colunas
+    return tabuleiro
+
+
+def ins_peça(tabuleiro, linha, coluna, peça):
+    #Inserir a peça no tabuleiro
+    tabuleiro[linha][coluna] = peça
+
+
+def val_local(tabuleiro,coluna):
+    #Validar a localização onde o Jogador quer inserir a peça - Pode ou não?
+    return tabuleiro[cont_linha-1][coluna] == 0 #se for true é ok inserir peça, se não significa que a coluna já foi "cheia" até ao de cima
+
+
+def verificar_prox_linha(tabuleiro,coluna): 
+    #verificar que row a peça vai/pode ser inserida; se o slot é 0, significa que ainda está vazio, por isso retorna o 1º index que está vazio
+    for l in range(cont_linha):
+        if tabuleiro[l][coluna] == 0:
+            return l
+
+
+def orientação_tabuleiro(tabuleiro):
+    print(np.flip(tabuleiro, 0)) #flip "vira" o tabuleiro consoante o x axis
+
+
+def vencedor (tabuleiro, peça):
+    #Verificar horizontais:
+    for c in range(cont_coluna-3):
+        for l in range(cont_linha):
+            if tabuleiro[l][c] == peça and tabuleiro[l][c+1] == peça and tabuleiro[l][c] == peça and tabuleiro[l][c+3] == peça:
+                return True
+
+    #Verificar Verticais
+    for c in range(cont_coluna):
+        for l in range(cont_linha-3):
+            if tabuleiro[l][c] == peça and tabuleiro[l+1][c] == peça and tabuleiro[l+2][c] == peça and tabuleiro[l+3][c] == peça:
+                return True
+
+    #Verificar Diagonais +
+    for c in range(cont_coluna-3):
+        for l in range(cont_linha-3):
+            if tabuleiro[l][c] == peça and tabuleiro[l+1][c+1] == peça and tabuleiro[l+2][c+2] == peça and tabuleiro[l+3][c+3] == peça:
+                return True
+
+    #Verificar Diagonais -
+    for c in range(cont_coluna-3):
+        for l in range(3, cont_linha):
+            if tabuleiro[l][c] == peça and tabuleiro[l-1][c+1] == peça and tabuleiro[l-2][c+2] == peça and tabuleiro[l-3][c+3] == peça:
+                return True
+
+
+def desenhar_tabuleiro(tabuleiro):
+    for c in range (cont_coluna):
+        for l in range (cont_linha):
+            pygame.draw.rect(screen, cor_tabuleiro,(c*secc_tabuleiro, l*secc_tabuleiro+secc_tabuleiro, secc_tabuleiro, secc_tabuleiro))
+            pygame.draw.circle(screen, cor_fundo, (int(c*secc_tabuleiro+secc_tabuleiro/2), int(l*secc_tabuleiro+secc_tabuleiro+secc_tabuleiro/2)), radius=int(secc_tabuleiro/2-5))
+
+    for c in range(cont_coluna):
+        for l in range(cont_linha):
+            if tabuleiro[l][c] == 1:
+                pygame.draw.circle(screen, verde, (int(c*secc_tabuleiro+secc_tabuleiro/2), height - int(l*secc_tabuleiro+secc_tabuleiro/2)), radius=int(secc_tabuleiro/2-5))
+            elif tabuleiro[l][c] == 2:
+                pygame.draw.circle(screen, rosa, (int(c*secc_tabuleiro+secc_tabuleiro/2), height - int(l*secc_tabuleiro+secc_tabuleiro/2)), radius=int(secc_tabuleiro/2-5))
+
+    pygame.display.update()
+
+
+
+pygame.init() #iniciar o jogo
+
+def inicio_jogo():
+    tabuleiro = criar_tabuleiro()
+    print(1)
+    fim_de_jogo = False
+    vez = 0
+
+    desenhar_tabuleiro(tabuleiro)
+    pygame.display.update()
+
+
+    print("Fim do jogo:"+str(fim_de_jogo))
+    while fim_de_jogo is not True:
+        
+        for event in pygame.event.get():
+            pygame.draw.rect(screen, cor_fundo, (0,0, width, secc_tabuleiro))
+            if event.type == pygame.QUIT: #fecha o jogo como deve ser, se se fechar a janela
+                pygame.quit()
+                sys.exit()
+            
+            if event.type == pygame.MOUSEMOTION:
+                posx = event.pos[0]
+                if vez == 0:
+                    pygame.draw.circle(screen, verde, (posx, int(secc_tabuleiro/2)), radius=int(secc_tabuleiro/2-5))
+                else: 
+                    pygame.draw.circle(screen, rosa, (posx, int(secc_tabuleiro/2)), radius=int(secc_tabuleiro/2-5))
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN: #todos os eventos do jogo, acontecem quando se clica "down" no rato
+                pygame.draw.rect(screen, cor_fundo, (0,0, width, secc_tabuleiro))
+                
+                #Jogador 1 joga:
+                if vez == 0: #inserir range de entrada de input
+                    posx = event.pos[0]
+                    coluna = int(math.floor(posx/secc_tabuleiro))
+
+                    if val_local(tabuleiro, coluna):
+                        linha = verificar_prox_linha(tabuleiro, coluna)
+                        ins_peça(tabuleiro, linha, coluna, 1)
+
+                        if vencedor(tabuleiro, 1):
+                            text_vencedor = font(35).render("Jogador 1 ganha!", True, verde)
+                            rect_menu = text_vencedor.get_rect(center=(350, 75))
+                            screen.blit(text_vencedor,rect_menu)
+                            fim_de_jogo = True
+
+                #Pedir o input do Jogador 2:
+                else:
+                    posx = event.pos[0]
+                    coluna = int(math.floor(posx/secc_tabuleiro))
+
+                    if val_local(tabuleiro, coluna):
+                        linha = verificar_prox_linha(tabuleiro, coluna)
+                        ins_peça(tabuleiro, linha, coluna, 2)
+
+                        if vencedor(tabuleiro, 2):
+                            text_vencedor = font(35).render("Jogador 2 ganha!", True, rosa)
+                            rect_menu = text_vencedor.get_rect(center=(350, 75))
+                            screen.blit(text_vencedor,rect_menu)
+                            fim_de_jogo = True
+
+                orientação_tabuleiro(tabuleiro)
+                desenhar_tabuleiro(tabuleiro)
+                    
+                vez +=1
+                vez = vez % 2 #alternar a vez apenas entre 0 e 1
+    return fim_de_jogo
---
+++ main.py
@@ -0,0 +1,68 @@
+import pygame, sys
+import jogo
+from button import Button
+
+
+pygame.init() #iniciar o jogo/janela
+
+#Variáveis Globais
+WINDOW = pygame.display.set_mode((700, 700))
+pygame.display.set_caption("4 em Linha")
+BG = pygame.image.load("assets\Background.png")
+
+BG_PLAY_BUTTON = pygame.image.load("assets\Play Rect.png")
+BG_AI_BUTTON = pygame.image.load("assets\Ai Rect.png")
+BG_EXIT_BUTTON = pygame.image.load("assets\Exit Rect.png")
+
+def font(size):
+    #Devolve a font no tamanho que eu quiser
+    return pygame.font.Font("assets/font.ttf", size)
+
+
+def play():
+    while True:
+        
+        fim_de_jogo=jogo.inicio_jogo()
+
+        if fim_de_jogo:
+            pygame.time.wait(1000)
+            menu_princ()
+
+        pygame.display.update()
+
+
+def menu_princ(): 
+    #Menu Principal
+    while True:
+        WINDOW.blit(BG, (0,0)) #blit - Block Transfer - copia os conteúdos de uma surface/window para outra
+
+        pos_mouse = pygame.mouse.get_pos() #get a posição do cursos do mouse
+        text_menu = font(75).render("MENU", True, "#f873a2")
+        rect_menu = text_menu.get_rect(center=(350, 75))
+
+        play_button = Button(image=pygame.transform.scale(BG_PLAY_BUTTON,(570,109) ), pos=(350, 250), text_input="JOGAR MULTIPLAYER", font=font(30), base_color="#d7fcd4", hovering_color="White")
+        ai_button = Button(image=pygame.transform.scale(BG_AI_BUTTON,(550,109) ), pos=(350, 400), text_input="JOGAR CONTRA AI", font=font(30), base_color="#d7fcd4", hovering_color="White")
+        exit_button = Button(image=pygame.transform.scale(BG_EXIT_BUTTON,(195,109) ), pos=(350, 550), text_input="SAIR", font=font(30), base_color="#d7fcd4", hovering_color="White")
+
+        WINDOW.blit(text_menu,rect_menu)
+
+        for button in [play_button, ai_button, exit_button]:
+            button.changeColor(pos_mouse)
+            button.update(WINDOW)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.checkForInput(pos_mouse): #se o input for no botão do "JOGAR" - começa o jogo multiplayer
+                    play()
+                #if ai_button.checkForInput(pos_mouse): #se o input for no botão do "JOGAR CONTRA AI" - começa o jogo contra AI
+                    #play_ai()
+                if exit_button.checkForInput(pos_mouse): #se o input for no botão do "SAIR" - a janela fecha-se/jogo encerra
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+menu_princ()
